8b4c4913daad27e23d129c823fa31e0483bb8e6c
MidnightDrifter <linkmasterchief@aol.com>
2018-02-15 01:38:49 -0800

    need final tweaks to get proj 3 working, bleh

--- mat_290_framework/mat290.cs
+++ mat_290_framework/mat290.cs
@@ -1031,7 +1031,7 @@ namespace mat_290_framework
                 //    P1DecastlejauCoef[0][i] = pts_[i];
                 //}
 
-                ////Re-calculate position based on cumulative sum -- double check this part, might have a < vs. <= error
+                ////Re-calculate position based on cumulative sum -- float check this part, might have a < vs. <= error
                 //for(int i=0;i<numCoef ;i++)
                 //{
                 //    for(int j=0;j<numCoef;j++)
@@ -1077,7 +1077,7 @@ namespace mat_290_framework
                 P1DecastlejauCoef[0][i] = pts_[i];
             }
 
-            //Re-calculate position based on cumulative sum -- double check this part, might have a < vs. <= error
+            //Re-calculate position based on cumulative sum -- float check this part, might have a < vs. <= error
             for (int i = 0; i < numCoef; i++)
             {
                 for (int j = 0; j < numCoef-i; j++)
@@ -1120,7 +1120,7 @@ namespace mat_290_framework
                 P2MidpointCoef[0][i] = pts_[i];
             }
 
-            //Re-calculate position based on cumulative sum -- double check this part, might have a < vs. <= error
+            //Re-calculate position based on cumulative sum -- float check this part, might have a < vs. <= error
             for (int i = 0; i < numCoef; i++)
             {
                 for (int j = 0; j < numCoef; j++)
@@ -1243,7 +1243,7 @@ namespace mat_290_framework
                     }
                 }
 
-                for (int offset = 1; offset < cPs.Count; offset++)   //Might be range-1 for these, double check
+                for (int offset = 1; offset < cPs.Count; offset++)   //Might be range-1 for these, float check
                 {
                     if (!(P2MidpointCoef[cPs.Count - offset][offset].x < 0 || P2MidpointCoef[cPs.Count - offset][offset].y < 0))
                     {
@@ -1342,7 +1342,7 @@ namespace mat_290_framework
                     outPoints.Add(tempTable[a][0]);
                 }
 
-                for(int offset =1;  offset< inputPoints.Count;  offset++)   //Might be range-1 for these, double check
+                for(int offset =1;  offset< inputPoints.Count;  offset++)   //Might be range-1 for these, float check
                 {
                     outPoints.Add(tempTable[inputPoints.Count - offset-1][offset]);
                 }
@@ -1357,11 +1357,37 @@ namespace mat_290_framework
         }
 
 
+        private List<float> TMinusVals(float inputT, int size)  //Up to, or up to and including size?
+        {
+
+            List<float> outList = new List<float>();
+            outList.Add(0);
+
+            if(size <=0)
+            {
+                return outList;
+            }
+
+            else
+            {
+                outList[0] = inputT;
+
+                for(int i=1;i<size;i++)
+                {
+                    outList.Add(outList[i - 1] * (inputT - i));
+                }
+
+
+                return outList;
+            }
+
+
+        }
 
         private Point2D PolyInterpolate(float t)
         {
             //return new Point2D(0, 0);
-
+            ClearDivDiffCoef();
             if(pts_.Count <2)
             {
                 return pts_[0];
@@ -1370,10 +1396,10 @@ namespace mat_290_framework
             else
             {
                 Point2D outPoint = DivDiffCoef(t,0,0);
-
+                List<float> tVals = TMinusVals(t, pts_.Count);
                 for(int i=1;i<pts_.Count;i++)
                 {
-                    outPoint += DivDiffCoef(t, 0, i) * PartialFactorial(pts_.Count, pts_.Count - i);
+                    outPoint += DivDiffCoef(t, 0, i) *tVals[i];
                 }
 
                 return outPoint;
@@ -1383,8 +1409,19 @@ namespace mat_290_framework
 
         }
 
+        private void ClearDivDiffCoef()
+        {
+            for(int i=0;i<P3PolyCoef.Count;i++)
+            {
+                for(int j=0;j<P3PolyCoef[i].Count;j++)
+                {
+                    P3PolyCoef[i][j].x = INVALID_COEF;
+                    P3PolyCoef[i][j].y = INVALID_COEF;
+                }
+            }
+        }
 
-        private Point2D DivDiffCoef(float t, int startIndex, int endIndex)
+        private Point2D DivDiffCoef(float t, int startIndex, int endIndex)  
         {
             if (P3PolyCoef[startIndex][endIndex].x != INVALID_COEF && P3PolyCoef[startIndex][endIndex].y != INVALID_COEF)
             {
@@ -1394,7 +1431,7 @@ namespace mat_290_framework
             else {
                 if (startIndex == endIndex)
                 {
-                    P3PolyCoef[startIndex][endIndex] = pts_[startIndex];
+                    P3PolyCoef[startIndex][0] = pts_[startIndex];
                     return pts_[startIndex];
                 }
 
@@ -1407,8 +1444,8 @@ namespace mat_290_framework
                     // return ( new Point2D(DivDiffCoef(t, startIndex + 1, endIndex).x - DivDiffCoef(t, startIndex, endIndex - 1).x, DivDiffCoef(t, startIndex + 1, endIndex).y - DivDiffCoef(t, startIndex, endIndex - 1).y) / (endIndex - startIndex);
 
 
-                    P3PolyCoef[startIndex][endIndex].x = (DivDiffCoef(t, startIndex + 1, endIndex).x - DivDiffCoef(t, startIndex, endIndex - 1).x) / (endIndex - startIndex);
-                    P3PolyCoef[startIndex][endIndex].y = (DivDiffCoef(t, startIndex + 1, endIndex).y - DivDiffCoef(t, startIndex, endIndex - 1).y) / (endIndex - startIndex);
+                    P3PolyCoef[startIndex][endIndex].x = (DivDiffCoef(t, startIndex+1, endIndex).x - DivDiffCoef(t, startIndex, endIndex-1).x) / (endIndex-startIndex);
+                    P3PolyCoef[startIndex][endIndex].y = (DivDiffCoef(t, startIndex+1, endIndex).y - DivDiffCoef(t, startIndex, endIndex-1).y) / (endIndex - startIndex);
 
 
 
